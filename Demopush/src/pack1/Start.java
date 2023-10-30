7a8b2b3a2475d2972396114af3114aaff940ba8e
lakshmikar Redyy <pallelakshmikarreddy12@gmail.com>
2023-10-30 11:59:44 +0530

    update

--- Demopush/src/pack1/Start.java
+++ Demopush/src/pack1/Start.java
@@ -5,6 +5,6 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("Welcome");
+         System.out.println("Second line");
 	}
-
 }
