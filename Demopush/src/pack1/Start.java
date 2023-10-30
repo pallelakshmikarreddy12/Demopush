0c56ec09332a1b39b27ef73ee3fece90e28cdaa9
pallelakshmikarreddy12 <146806701+pallelakshmikarreddy12@users.noreply.github.com>
2023-10-30 12:07:20 +0530

    Update Start.java

--- Demopush/src/pack1/Start.java
+++ Demopush/src/pack1/Start.java
@@ -6,5 +6,6 @@ public class Start {
 		// TODO Auto-generated method stub
          System.out.println("Welcome");
          System.out.println("Second line");
+	 System.out.println("Second line");
 	}
 }
